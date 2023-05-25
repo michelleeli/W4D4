e33d75c509e7a2345851b2d0dd13dfe4fcfe630f
Michelle <rphmichelle@gmail.com>
2023-05-25 15:22:20 -0400

    finished anagram project

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/anagram.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/anagram.rb
@@ -28,3 +28,39 @@ end
 p second_anagram?("hello", "cat")
 p second_anagram?("catt", "cat")
 p second_anagram?("atc", "cat")
+
+def third_anagram?(word_1, word_2)
+  word_1.split("").sort == word_2.split("").sort
+end 
+
+puts
+p third_anagram?("hello", "cat")
+p third_anagram?("catt", "cat")
+p third_anagram?("atc", "cat")
+
+def fourth_anagram?(word_1, word_2)
+  anagram_helper(word_1) == anagram_helper(word_2)
+end
+
+def anagram_helper(word)
+  hash = Hash.new(0)
+  word.each_char { |char| hash[char] += 1 }
+  hash
+end 
+
+puts
+p fourth_anagram?("hello", "cat")
+p fourth_anagram?("catt", "cat")
+p fourth_anagram?("atc", "cat")
+
+def bonus_anagram?(word_1, word_2)
+  hash = Hash.new(0)
+  word_1.each_char { |char| hash[char] += 1 }
+  word_2.each_char { |char| hash[char] -= 1}
+  hash.values.all? { |val| val == 0}
+end 
+
+puts
+p bonus_anagram?("hello", "cat")
+p bonus_anagram?("catt", "cat")
+p bonus_anagram?("atc", "cat")
