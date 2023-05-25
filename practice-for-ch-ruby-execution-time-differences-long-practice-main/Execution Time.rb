8d7dd6842e7a9aa52b2dbe3375c0cd251b360883
XLG123 <xiaolin.guan123@gmail.com>
2023-05-25 14:39:39 -0400

    Execution time phase 1 & 2 done

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/Execution Time.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/Execution Time.rb
@@ -8,6 +8,7 @@ def my_min(lists)
 end 
 
 puts my_min(list)
+puts
 
 def my_min2(lists)
     min = 0
@@ -18,6 +19,7 @@ def my_min2(lists)
 end 
 
 puts my_min2(list)
+puts
 
 def largest_contiguous_subsum(list)
     subs = []
@@ -36,14 +38,52 @@ end
 arr = [5,3,-7]
 arr2 = [2,3,-6, 7, -6, 7]
 p largest_contiguous_subsum(arr2)
+puts
 
 def largest_contiguous_subsum2(list)
-    sub = []
-    max = list[0]
-    i = list.length-1
-    while i >= 0 
-    if !sub.empty?
-        sub << list[0..i]
-        i -=1 
-    end 
-end 
+  positive_nums = []
+  negative_nums = []
+  list.each  { |ele| ele > 0 ? positive_nums << ele : negative_nums << ele }
+
+  if negative_nums.empty?
+    return positive_nums.sum
+  elsif positive_nums.empty?
+    greatest_sum = negative_nums.first
+    negative_nums.each { |ele| greatest_sum = ele if ele > greatest_sum }
+    return greatest_sum
+  else
+    largest_sum = 0
+    curr_largest_sum = 0
+
+    i = 0
+    k = 0
+
+    list_length = list.length
+    while i < list_length
+      curr_largest_sum += list[i]
+      if curr_largest_sum > largest_sum
+        largest_sum = curr_largest_sum
+      end
+
+      if i == list_length - 1
+        k += 1
+        i = 0
+        i += k
+        curr_largest_sum = 0
+      end
+      
+      if k == list_length - 1
+        return largest_sum
+      end
+
+      i += 1
+
+    end
+  end
+end
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum2(list) # => -1 (from [-1])
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum2(list) # => 8 (from [7, -6, 7])
